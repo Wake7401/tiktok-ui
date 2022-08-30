1b5ca0476b59c2cef0f096cc0b7a24e2b84ef543
Wake7401 <91364482+Wake7401@users.noreply.github.com>
2022-08-30 14:25:47 +0700

    xay dung header ui #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,13 +1,30 @@
 import classNames from 'classnames/bind'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons'
 
 import styles from './Header.module.scss'
-
+import images from '~/assets/images'
 const cx = classNames.bind(styles)
 
 function Header() {
     return <header className={cx('wrapper')}>
         <div className={cx('inner')}>
-
+            <div className={cx('logo')}>
+                <img src={images.logo} alt="TikTok" />
+            </div>
+            <div className={cx('search')}>
+                <input type="text" placeholder='Search account and videos ' spellCheck={false} />
+                <button className={cx('clear')}>
+                    <FontAwesomeIcon icon={faCircleXmark} />
+                </button>
+                <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                <button className={cx('btn-search')}>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </button>
+            </div>
+            <div className={cx('action')}>
+                <h1>Upload</h1>
+            </div>
         </div>
     </header>
 }
